9004abd05951aab0aeed05fbc43cd32b72b2eddd
omkargaikwad23 <omkargaikwad9552@gmail.com>
2022-05-15 00:03:18 +0530

    tracing path in astar

--- Assignment_02_a_star/A_Star.cpp
+++ Assignment_02_a_star/A_Star.cpp
@@ -3,7 +3,7 @@ using namespace std;
 
 struct Cell {
     int parenti, parentj;
-    double f, g, h;
+    double f, g, h; // f = g + h
 };
 
 class A_Star {
@@ -16,6 +16,7 @@ public:
         this->path = path;
         src = {0,0};
         dest = {4,2};
+        path[src.first][src.second] = 'S';
     }
 
     void printPath(){
@@ -26,12 +27,22 @@ public:
         }
     }
 
+    void tracePath(vector<vector<Cell>>& cellInfo){
+        int x = dest.first, y = dest.second;
+        while(!(cellInfo[x][y].parenti == -1 && cellInfo[x][y].parentj == -1)){
+            path[x][y] = '$';
+            int tempx = cellInfo[x][y].parenti;
+            int tempy = cellInfo[x][y].parentj;
+            x = tempx, y = tempy;
+        }
+        path[src.first][src.second] = 'S';
+    }
+
     void A_StarSearch(){
         bool closeList[R][C];
         memset(closeList, false, sizeof(closeList));
         using pdii = pair<double, pair<int, int>>;
         priority_queue<pdii, vector<pdii>, greater<pdii>> openList; // <f,<i,j>> 
-
         vector<vector<Cell>> cellInfo;
         cellInfo.resize(R, vector<Cell>(C));
         for(int i=0; i<R; i++){
@@ -43,13 +54,13 @@ public:
                 cellInfo[i][j].parentj = -1;
             }
         }
-        // starting node
+
         int i = src.first, j = src.second;
         cellInfo[i][j].f = 0.0;
         cellInfo[i][j].g = 0.0;
         cellInfo[i][j].h = 0.0;
-        cellInfo[i][j].parenti = i;
-        cellInfo[i][j].parentj = j; 
+        // cellInfo[i][j].parenti = i;
+        // cellInfo[i][j].parentj = j; 
         vector<pair<int, int>> dir = {{0, 1},{1,0},{-1,0},{0,-1}};
         openList.push({0.0, {i, j}});
         bool foundDest = false;
@@ -61,15 +72,14 @@ public:
             int curj = p.second.second;
             closeList[curi][curj] = true;
             path[curi][curj] = '+';
-            if(foundDest) continue;
-
+            if(curi==dest.first && curj==dest.second){ 
+                foundDest = true; break;
+            }
+    
             for(auto& d : dir){
                 int newi = curi + d.first;
                 int newj = curj + d.second;
                 if(newi<0 || newj<0 || newi>=R || newj>=C) continue;
-                if(newi==dest.first && newj==dest.second){
-                    foundDest = true;
-                }
                 if(path[newi][newj]=='*' || closeList[newi][newj]) continue;
 
                 int gNew = cellInfo[i][j].g + 1.0;
@@ -81,22 +91,22 @@ public:
                     cellInfo[newi][newj].g = gNew;
                     cellInfo[newi][newj].h = hNew;
                     cellInfo[newi][newj].f = fNew;
-                    cellInfo[newi][newj].parenti = i;
-                    cellInfo[newi][newj].parentj = j;
+                    cellInfo[newi][newj].parenti = curi;
+                    cellInfo[newi][newj].parentj = curj;
                 }
             }
         }
         if(foundDest){
+            tracePath(cellInfo);
             cout << "Found my destiny!!\n";
             printPath();
-        }else{
+        }else
             cout << "Cannot reach destiny!!\n";
-        }
     }
 };
 
 int main(){
-    vector<vector<char>> path = {{'.','.','.','.','.'},{'.','.','.','.','.'},{'.','.','.','.','.'},{'*','*','*','*','.'},{'D','.','.','.','.'}};
+    vector<vector<char>> path = {{'.','.','.','.','.'},{'.','.','.','.','.'},{'.','.','.','.','.'},{'*','*','*','*','.'},{'.','.','.','.','.'}};
     A_Star astar(path);
     astar.A_StarSearch();
 }
